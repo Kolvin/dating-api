e5881a384254fb1950f6c904131977a543f7270f
Kolvin <taylorcalvin07@gmail.com>
2021-10-26 19:33:04 +0100

    feat: created migration for new basic User entity

--- app/src/Modules/Users/Entities/User.php
+++ app/src/Modules/Users/Entities/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Modules\Users\Entities;
 
+use Doctrine\ORM\Mapping\ClassMetadata;
+
 class User
 {
     private string $id;
@@ -10,5 +12,19 @@ class User
 
     public function __construct()
     {
+    }
+
+    public static function loadMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->mapField([
+            'id' => true,
+            'fieldName' => 'id',
+            'type' => 'integer',
+        ]);
+
+        $metadata->mapField([
+            'fieldName' => 'email',
+            'type' => 'string',
+        ]);
     }
 }
