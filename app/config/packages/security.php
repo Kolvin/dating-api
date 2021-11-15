2054fd130e90b5141c5f5a675644174d2012a1c0
Kolvin <taylorcalvin07@gmail.com>
2021-11-15 18:05:04 +0000

    chore: set gaurd handlers to use vendors

--- app/config/packages/security.php
+++ app/config/packages/security.php
@@ -2,6 +2,8 @@
 
 use App\Modules\Users\Entities\User;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationFailureHandler;
 use Symfony\Config\SecurityConfig;
 
 return static function (SecurityConfig $security) {
@@ -20,19 +22,21 @@ return static function (SecurityConfig $security) {
         ->security(false)
     ;
 
-    $security->firewall('api_login')
+    $security->firewall('login')
         ->pattern('^/api/user/login')
         ->stateless(true)
         ->lazy(true)
         ->jsonLogin()
-            ->checkPath('api_login')
-                ->usernamePath('email')
-                ->passwordPath('password')
+            ->checkPath('/api/user/login')
+            ->usernamePath('email')
+            ->passwordPath('password')
+            ->successHandler(AuthenticationSuccessHandler::class)
+            ->failureHandler(AuthenticationFailureHandler::class)
     ;
 
-//    $security->firewall('api')
-//        ->pattern('^/api/v')
-//        ->stateless(true)
-//        ->guard()
-//    ;
+    $security->firewall('api')
+        ->pattern('^/api/v')
+        ->stateless(true)
+        ->jwt()
+    ;
 };
